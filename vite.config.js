633ff24d4a5cdc8795c219dbb94ef3f8ef547282
shreshta-2008 <shreshtapapineni@gmail.com>
2026-02-19 00:52:58 +0530

    tjhird

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/week.04/",
+ base: "/week.04/", //YourRepoName
 })
